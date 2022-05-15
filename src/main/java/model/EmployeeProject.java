bcb5cebbdb61642cf5b474b870380f4b7f42efe8
Ott Pikk <ott@karmai.eu>
2022-05-15 08:59:29 +0300

    Department Project EmployeeProject

--- src/main/java/model/EmployeeProject.java
+++ src/main/java/model/EmployeeProject.java
@@ -1,5 +1,40 @@
 package model;
 
 public class EmployeeProject {
+    private int employeeProject;
+    private int employeeId;
+    private int projectId;
 
+    public int getEmployeeProject() {
+        return employeeProject;
+    }
+
+    public void setEmployeeProject(int employeeProject) {
+        this.employeeProject = employeeProject;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeProject{" +
+                "employeeProject=" + employeeProject +
+                ", employeeId=" + employeeId +
+                ", projectId=" + projectId +
+                '}';
+    }
 }
